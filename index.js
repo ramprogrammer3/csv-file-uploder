7889db7a9c651b3261ed3d8ef2c577b4a26871ef
ram kumar <rammaniyari@gmail.com>
2023-09-12 10:48:25 +0530

    installing some package and activating home route

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ require("dotenv").config();
 const dbConnect = require("./config/dbConnection");
 const path = require("path");
 
+const expressLayouts = require('express-ejs-layouts');
+const csv = require("csv-parser");
+const bodyParser = require("body-parser");
+
 
 const port = process.env.port || 8080;
 
@@ -12,12 +16,24 @@ const app = express();
 // connecting to mongodb atlas
 dbConnect();
 
+
+// setting layouts
+app.use(expressLayouts);
+
+// middleware for body parser
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }))
+
 // accessing static files from assets folder
-app.use(express.static(__dirname +'/assets')); 
+app.use(express.static(__dirname + '/assets'));
 
 // setting up view engine
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+
+
+// setting up routes
+app.use("/", require("./routes/index"))
 
 
 // server activate
