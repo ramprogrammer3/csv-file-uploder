b6d09cacc87b92f7ef9e6005921c94bcf35cc785
ram kumar <rammaniyari@gmail.com>
2023-09-12 12:43:17 +0530

    change in home controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,14 @@
+const File = require("../model/csv");
 
 module.exports.home = async(req,res)=>{
-    return res.render("home",{
-        title : "Home"
-    })
+    try {
+        let file = await File.find({});
+        return res.render('home', {
+            files: file,
+            title: "Home"
+        });
+    } catch (error) {
+        console.log('Error in homeController/home', error);
+        return;
+    }
 }
