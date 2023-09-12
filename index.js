a2f221b99c8711d2f111d09b146e6892e978a143
ram kumar <rammaniyari@gmail.com>
2023-09-12 10:37:44 +0530

    setting views engine express static and home router

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 require("dotenv").config();
 const dbConnect = require("./config/dbConnection");
+const path = require("path");
 
 
 const port = process.env.port || 8080;
@@ -10,6 +11,13 @@ const app = express();
 
 // connecting to mongodb atlas
 dbConnect();
+
+// accessing static files from assets folder
+app.use(express.static(__dirname +'/assets')); 
+
+// setting up view engine
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"))
 
 
 // server activate
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,14 @@ const express = require('express');
 const router = express.Router();
 
 
+// importing controllers
+const { home } = require("../controllers/home_controller");
+
+
+// making routes
+router.get("/",home);
+
+
 
 // exporting router
 module.exports = router;
